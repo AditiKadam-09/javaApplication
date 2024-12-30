18199422e215c730acb692ee7021cadfa699be79
AditiKadam-09 <aditikadam2005@gmail.com>
2024-12-30 13:06:08 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String args[]){
         System.out.println("Sum of 10 and 20 is :");
-        System.out.println("10 + 20 = "+(10+20));
+        System.out.println("10 + 20 = "+(1+2));
     }
 }
